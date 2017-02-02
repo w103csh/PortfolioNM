1cadaffafe8be151ea6ccf8825a80a083d958912
w103csh <cloiniffty@gmail.com>
2017-02-01 20:11:03 -0700

    second commit

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+// test a small change
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
