d383bab90ad42766f97d16541d8f7d0b01ef535a
w103csh <cloiniffty@gmail.com>
2017-02-01 21:04:12 -0700

    test password in vs code

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-// hey im back again
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
