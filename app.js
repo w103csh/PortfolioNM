08a4dde61122346ff138e67b3f0725cd54b27950
w103csh <cloiniffty@gmail.com>
2017-02-09 19:05:59 -0700

    commit back on master branch

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-// I am going to uncommit this
+// here is a change made back on master
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
