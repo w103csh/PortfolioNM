a50e42c01c9a456422cd6b6ada3d9e0c4244f1f9
w103csh <cloiniffty@gmail.com>
2017-04-23 19:39:21 -0600

    validation got lost. added it back in.

--- client/app/components/sign-in/sign-in.component.ts
+++ client/app/components/sign-in/sign-in.component.ts
@@ -1,24 +1,30 @@
 
-import { Component,
-         OnInit }             from '@angular/core';
-import { Router,
-         ActivatedRoute,
-         NavigationExtras }   from '@angular/router';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  Router,
+  ActivatedRoute,
+  NavigationExtras
+} from '@angular/router';
 
-import { AuthService }        from '../../../shared-services/auth.service';
-import { User }               from '../../../models/User';
-import { ResponseData }       from '../../../models/ResponseData';
+import { AuthService } from '../../../shared-services/auth.service';
+import { User } from '../../../models/User';
+import { ResponseData } from '../../../models/ResponseData';
 
 @Component({
   moduleId: module.id,
   selector: 'sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: [ '../../../shared-css/form.css', './sign-in.component.css' ]
+  styleUrls: ['../../../shared-css/form.css', './sign-in.component.css']
 })
-export class SignInComponent{
+export class SignInComponent {
 
-  // layout
-  private isPopup: boolean;
+  // validation
+  private readonly emailRegex: RegExp = new RegExp('^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$');
+  private emailVal: string;
+  private passwordVal: string;
 
   private redirectUrl: string;
   private model: User;
@@ -26,7 +32,6 @@ export class SignInComponent{
 
   constructor(private authService: AuthService, private route: ActivatedRoute, public router: Router) {
     // defaults
-    this.isPopup = false;
     this.model = new User('', '', '');
   }
 
@@ -35,30 +40,64 @@ export class SignInComponent{
     this.redirectUrl = this.route.snapshot.queryParams['redirectUrl'] || '/dashboard';
   }
 
+  validate(): boolean {
+    // TODO: simplify the validation
+    let valid = true;
+
+    // email validation
+    if (!this.model.email || this.model.email.length < 1) {
+      this.emailVal = 'required';
+      valid = false;
+    }
+    else if (!this.emailRegex.test(this.model.email)) {
+      this.emailVal = 'email';
+      valid = false;
+    }
+    else {
+      this.emailVal = null;
+    }
+
+    // password validation
+    if (!this.model.password) {
+      this.passwordVal = 'required';
+      valid = false;
+    }
+    else if (this.model.password.length < 7) {
+      this.passwordVal = 'length';
+      valid = false;
+    }
+    else {
+      this.passwordVal = null;
+    }
+
+    return valid;
+  }
+
   onSubmit() {
-    // TODO: loading
+    if (this.validate()) {
 
-    this.authService.signin(this.model).subscribe(
-      (response) => {
-        // TODO: move common logic
-        if(response && response.success) {
-          this.serverMsg = null;
-          this.router.navigate([this.redirectUrl]);
+      this.authService.signin(this.model).subscribe(
+        (response) => {
+          // TODO: move common logic
+          if (response && response.success) {
+            this.serverMsg = null;
+            this.router.navigate([this.redirectUrl]);
+          }
+          // show message from the server
+          else {
+            // // TODO: log
+            // if (!response.message) console.log('Unknown issue. Contact an administrator.')
+            this.serverMsg = response.message || 'Unknown issue. Contact an administrator.';
+          }
+        },
+        (err) => {
+          // TODO: log & something else
+          console.log(err);
+          this.serverMsg = 'Server error please try again. If the problem continues contact an administrator.';
         }
-        // show message from the server
-        else {
-          // // TODO: log
-          // if (!response.message) console.log('Unknown issue. Contact an administrator.')
-          this.serverMsg = response.message || 'Unknown issue. Contact an administrator.';
-        }
-      },
-      (err) => {
-        // TODO: log & something else
-        console.log(err);
-        this.serverMsg = 'Server error please try again. If the problem continues contact an administrator.';
-      }
-    );
+      );
 
+    }
   }
 
 }
--- client/app/components/sign-up/sign-up.component.ts
+++ client/app/components/sign-up/sign-up.component.ts
@@ -18,9 +18,14 @@ import {
   moduleId: module.id,
   selector: 'sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: [ '../../../shared-css/form.css', './sign-up.component.css' ]
+  styleUrls: ['../../../shared-css/form.css', './sign-up.component.css']
 })
-export class SignUpComponent{
+export class SignUpComponent {
+
+  // validation
+  private readonly emailRegex: RegExp = new RegExp('^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$');
+  private emailVal: string;
+  private passwordVal: string;
 
   private model: User;
   private serverMsg: string;
@@ -30,28 +35,63 @@ export class SignUpComponent{
     this.model = new User('', '', '');
   }
 
+  validate(): boolean {
+    // TODO: simplify the validation
+    let valid = true;
+
+    // email validation
+    if (!this.model.email || this.model.email.length < 1) {
+      this.emailVal = 'required';
+      valid = false;
+    }
+    else if (!this.emailRegex.test(this.model.email)) {
+      this.emailVal = 'email';
+      valid = false;
+    }
+    else {
+      this.emailVal = null;
+    }
+
+    // password validation
+    if (!this.model.password) {
+      this.passwordVal = 'required';
+      valid = false;
+    }
+    else if (this.model.password.length < 7) {
+      this.passwordVal = 'length';
+      valid = false;
+    }
+    else {
+      this.passwordVal = null;
+    }
+
+    return valid;
+  }
+
   onSubmit() {
+    if (this.validate()) {
 
-    this.userService.create(this.model).subscribe(
-      response => {
-        if(response.success) {
-          this.serverMsg = null;
-          this.router.navigate(['signin']);
+      this.userService.create(this.model).subscribe(
+        response => {
+          if (response.success) {
+            this.serverMsg = null;
+            this.router.navigate(['signin']);
+          }
+          // show message from the server
+          else {
+            // // TODO: log
+            // if (!response.message) console.log('Unknown issue. Contact an administrator.')
+            this.serverMsg = response.message || 'Unknown issue. Contact an administrator.';
+          }
+        },
+        err => {
+          // TODO: log & something else
+          console.log(err);
+          this.serverMsg = 'Server error please try again. If the problem continues contact an administrator.';
         }
-        // show message from the server
-        else {
-          // // TODO: log
-          // if (!response.message) console.log('Unknown issue. Contact an administrator.')
-          this.serverMsg = response.message || 'Unknown issue. Contact an administrator.';
-        }
-      },
-      err => {
-        // TODO: log & something else
-        console.log(err);
-        this.serverMsg = 'Server error please try again. If the problem continues contact an administrator.';
-      }
-    );
+      );
 
+    }
   }
 
 }
