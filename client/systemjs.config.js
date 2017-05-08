b05a7f2e896e80bc644ef84c4dd70ac7fd5410ce
w103csh <cloiniffty@gmail.com>
2017-05-07 20:26:33 -0600

    more polyfill

--- client/systemjs.config.js
+++ client/systemjs.config.js
@@ -49,7 +49,7 @@
       'rxjs':                       'npm:rxjs@5.0.1',
       'angular-in-memory-web-api':  'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js',
       'ts':                         'npm:plugin-typescript@5.2.7/lib/plugin.js',
-      'typescript':                 'npm:typescript@2.0.10/lib/typescript.js',
+      'typescript':                 'npm:typescript@2.3.2/lib/typescript.js',
       'hammerjs':                   'npm:hammerjs@2.0.8/hammer.js',
 
       // 'angular2-jwt':               'node_modules/angular2-jwt',
